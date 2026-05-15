78803a8273150be19572f4f67c5253f7e62cbc50
udaykiran <udaybandi15@gmail.com>
2026-05-15 12:31:53 +0000

    Fix ESLint arrow body style errors

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -11,7 +11,7 @@ const FiltersGroup = props => {
     employmentTypesList,
     changeSalary,
     salaryRangesList,
-    changeLocation, // We added this prop to handle location clicks
+    changeLocation,
   } = props
 
   const onChangeSearchInput = event => {
@@ -24,104 +24,98 @@ const FiltersGroup = props => {
     }
   }
 
-  const renderSearchInput = () => {
-    return (
-      <div className="search-input-container">
-        <label htmlFor="searchInput" className="visually-hidden">
-          Search
-        </label>
-        <input
-          type="search"
-          id="searchInput"
-          className="search-input"
-          placeholder="Search"
-          value={searchInput}
-          onChange={onChangeSearchInput}
-          onKeyDown={onEnterSearchInput}
-        />
-        <button
-          type="button"
-          data-testid="searchButton"
-          className="search-button-container"
-          onClick={getJobs}
-        >
-          <BsSearch className="search-icon" />
-        </button>
-      </div>
-    )
-  }
+  // FIXED: Implicit return using () instead of { return () }
+  const renderSearchInput = () => (
+    <div className="search-input-container">
+      <label htmlFor="searchInput" className="visually-hidden">
+        Search
+      </label>
+      <input
+        type="search"
+        id="searchInput"
+        className="search-input"
+        placeholder="Search"
+        value={searchInput}
+        onChange={onChangeSearchInput}
+        onKeyDown={onEnterSearchInput}
+      />
+      <button
+        type="button"
+        data-testid="searchButton"
+        className="search-button-container"
+        onClick={getJobs}
+      >
+        <BsSearch className="search-icon" />
+      </button>
+    </div>
+  )
 
-  const renderTypeOfEmployment = () => {
-    return (
-      <div className="employment-type-container">
-        <h1 className="employment-type-heading">Type of Employment</h1>
-        <ul className="employee-type-list-container">
-          {employmentTypesList.map(eachEmployeeType => {
-            const onSelectEmployeeType = event => {
-              changeEmployeeList(event.target.value)
-            }
-            return (
-              <li
-                className="employee-item"
-                key={eachEmployeeType.employmentTypeId}
+  // FIXED: Implicit return using ()
+  const renderTypeOfEmployment = () => (
+    <div className="employment-type-container">
+      <h1 className="employment-type-heading">Type of Employment</h1>
+      <ul className="employee-type-list-container">
+        {employmentTypesList.map(eachEmployeeType => {
+          const onSelectEmployeeType = event => {
+            changeEmployeeList(event.target.value)
+          }
+          return (
+            <li
+              className="employee-item"
+              key={eachEmployeeType.employmentTypeId}
+            >
+              <input
+                type="checkbox"
+                id={eachEmployeeType.employmentTypeId}
+                className="check-input"
+                value={eachEmployeeType.employmentTypeId}
+                onChange={onSelectEmployeeType}
+              />
+              <label
+                htmlFor={eachEmployeeType.employmentTypeId}
+                className="check-label"
               >
-                <input
-                  type="checkbox"
-                  id={eachEmployeeType.employmentTypeId}
-                  className="check-input"
-                  value={eachEmployeeType.employmentTypeId}
-                  onChange={onSelectEmployeeType}
-                />
-                <label
-                  htmlFor={eachEmployeeType.employmentTypeId}
-                  className="check-label"
-                >
-                  {eachEmployeeType.label}
-                </label>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  }
+                {eachEmployeeType.label}
+              </label>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 
-  const renderSalaryRange = () => {
-    return (
-      <div className="salary-range-container">
-        <h1 className="salary-range-heading">Salary Range</h1>
-        <ul className="salary-range-list-container">
-          {salaryRangesList.map(eachSalary => {
-            const onClickSalary = () => {
-              changeSalary(eachSalary.salaryRangeId)
-            }
-            return (
-              <li
-                className="salary-item"
-                key={eachSalary.salaryRangeId}
-                onClick={onClickSalary}
-              >
-                <input
-                  type="radio"
-                  id={eachSalary.salaryRangeId}
-                  name="salary"
-                  className="check-input"
-                />
-                <label
-                  htmlFor={eachSalary.salaryRangeId}
-                  className="check-label"
-                >
-                  {eachSalary.label}
-                </label>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  }
+  // FIXED: Implicit return using ()
+  const renderSalaryRange = () => (
+    <div className="salary-range-container">
+      <h1 className="salary-range-heading">Salary Range</h1>
+      <ul className="salary-range-list-container">
+        {salaryRangesList.map(eachSalary => {
+          const onClickSalary = () => {
+            changeSalary(eachSalary.salaryRangeId)
+          }
+          return (
+            <li
+              className="salary-item"
+              key={eachSalary.salaryRangeId}
+              onClick={onClickSalary}
+            >
+              <input
+                type="radio"
+                id={eachSalary.salaryRangeId}
+                name="salary"
+                className="check-input"
+              />
+              <label htmlFor={eachSalary.salaryRangeId} className="check-label">
+                {eachSalary.label}
+              </label>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 
-  // NEW FUNCTION: Renders the Locations exactly as the tests expect
+  // This one is fine to keep the block { } because it has logic (onChangeLocation) inside it
   const renderLocations = () => {
     const onChangeLocation = event => {
       changeLocation(event.target.value, event.target.checked)
@@ -167,7 +161,6 @@ const FiltersGroup = props => {
       <hr className="horizontal-line" />
       {renderTypeOfEmployment()}
       <hr className="horizontal-line" />
-      {/* Call the new locations function here */}
       {renderLocations()}
       <hr className="horizontal-line" />
       {renderSalaryRange()}
